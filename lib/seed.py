79d328efaf6e84005fe49d9fe71d01206d757899
Justin Rodriguez <justin.r@me.com>
2024-06-04 07:31:38 -0400

    drop and create tables in seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
 from models.__init__ import CONN, CURSOR
+from models.Artist import Artist
+from models.Concert import Concert
+
+def main():
+    Artist.drop_table()
+    Concert.drop_table()
+    Artist.create_table()
+    Concert.create_table()
