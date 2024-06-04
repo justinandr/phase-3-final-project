a8390684d8c05b2e692009abd231565f634d4ed8
Justin Rodriguez <justin.r@me.com>
2024-06-03 20:21:01 -0400

    add Concert.py  __init__ and __repr__ methods

--- lib/models/Concert.py
+++ lib/models/Concert.py
@@ -2,8 +2,24 @@ from models.__init__ import CURSOR, CONN
 import datetime
 
 class Concert:
-    def __init__():
-        pass
+
+    all = {}
+
+    def __init__(self, tour, date, city, venue, artist_id, id = None):
+        self.id = id
+        self.tour = tour
+        self.date = date
+        self.city = city
+        self.venue = venue
+        self.artist_id = artist_id
+
+    def __repr__(self):
+        return f'Tour: {self.tour}' + \
+            f'Date: {self.date}' + \
+            f'City: {self.city}' + \
+            f'Venue: {self.venue}' + \
+            f'Artist ID: {self.artist_id}'
+
 
     @classmethod
     def instance_from_db(cls):
